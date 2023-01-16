9deace4054146a556a27cfe7267929806e4d03bb
mouhamed <coulibalymouhamed27@gmail.com>
2023-01-16 00:02:06 +0000

    depot:first add project

---
+++ XP/src/app/my-app/product/product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+name ="Web Developer";
+price = 400;
+}
